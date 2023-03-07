489d072e23607fac25b07ea8c5f00aef2465dc7f
Oguzhan AYDOGAN <oguzhan.aydogan@deop.ca>
2023-03-07 14:43:44 -0500

    acr added

--- main.tf
+++ main.tf
@@ -24,6 +24,13 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 
+resource "azurerm_container_registry" "acr" {
+  name                = var.acr_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Standard"
+}
+
 
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = "${var.prefix}-aks"
@@ -40,4 +47,14 @@ resource "azurerm_kubernetes_cluster" "aks" {
   identity {
      type = "SystemAssigned"
   }
+}
+
+resource "azurerm_role_assignment" "role_assignment" {
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.acr.id
+  skip_service_principal_aad_check = true
+  depends_on = [
+    azurerm_kubernetes_cluster.aks
+  ]
 }
